b22aef9d6485a5f90eef53d746d39222862d5e59
huangpeilin <huangpeilin@qunjielong.com>
2022-04-26 18:04:46 +0800

    feat: 增加停止脚本

---
+++ stop.sh
@@ -0,0 +1,10 @@
+tsc
+NAME=${2:-server-webhook};
+npx pm2 describe $NAME > /dev/null
+RUNNING=$?
+if [ "${RUNNING}" -ne 0 ]; then
+    echo "not app '$NAME'..."
+else
+    echo "stop app '$NAME'..."
+    npx pm2 stop $NAME
+fi;
