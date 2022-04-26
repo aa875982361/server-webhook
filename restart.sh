18fb6be0ce673aee830c6b874a1f25fec471172b
huangpeilin <huangpeilin@qunjielong.com>
2022-04-26 17:33:45 +0800

    feat: 完善重启命令

--- restart.sh
+++ restart.sh
@@ -1,10 +1,11 @@
+tsc
 NAME=$1;
 npx pm2 describe $NAME > /dev/null
 RUNNING=$?
 if [ "${RUNNING}" -ne 0 ]; then
     echo "start app '$NAME'..."
+    npx pm2 start ./dist/index.js --name $NAME
 else
     echo "restart app '$NAME'..."
-    npm run _stop
+    npx pm2 restart $NAME
 fi;
-npm run start
